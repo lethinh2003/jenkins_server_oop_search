64c2ac55fdf71621ef808ffb9aef5b2b509b1985
lethinh2003 <muradvn2003@gmail.com>
2023-11-24 13:42:26 +0700

    sfsa

--- configs/config.endpoint.js
+++ configs/config.endpoint.js
@@ -1,19 +1,25 @@
+const defaultPort = 8081;
 const development = {
   server: {
-    port: process.env.DEV_PORT,
+    port: process.env.DEV_PORT || defaultPort,
+  },
+};
+const test = {
+  server: {
+    port: process.env.STAGING_PORT || defaultPort,
   },
 };
 const staging = {
   server: {
-    port: process.env.STAGING_PORT,
+    port: process.env.STAGING_PORT || defaultPort,
   },
 };
 const production = {
   server: {
-    port: process.env.PRO_PORT,
+    port: process.env.PRO_PORT || defaultPort,
   },
 };
 
-const config = { development, staging, production };
+const config = { development, staging, production, test };
 
 module.exports = config[process.env.NODE_ENV || "development"];
